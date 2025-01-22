b6f97847659c0edc78decc42078f271161d3df18
hollowM1ke <mihaiadauji12@gmail.com>
2025-01-22 16:54:01 +0200

    Revert "added loading command files logic"
    This reverts commit b2f46ac0930b4c234d8b013523484456b41d6ab8.

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import 'dotenv/config';
-import fs from 'fs';
-import path from 'path';
-
 
 const token = process.env.TOKEN;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [] });
 
 client.once(Events.ClientReady, readyClient => {
   console.log('Ready!', `Logged in as ${readyClient.user.tag}`);
@@ -19,24 +16,3 @@ client.on('interactionCreate', (interaction) => {
 });
 
 client.login(token);
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = await import(filePath);
-
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command)
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" propriety.`);
-    }
-
-  }
-}
