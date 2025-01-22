350799d11bf54e075bece392fa6d14addfbfc460
hollowM1ke <mihaiadauji12@gmail.com>
2025-01-22 14:11:32 +0200

    created example commands from the guide

---
+++ commands/utility/server.js
@@ -0,0 +1,11 @@
+import { SlashCommanderBuilder } from 'discord.js';
+
+export const command = {
+    data: new SlashCommanderBuilder()
+        .setName('server')
+        .setDescription('Provides information about the server.'),
+    async execute (interaction) {
+        await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+    }
+
+}
