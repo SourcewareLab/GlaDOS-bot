bc433a4342c089c69049ba70274460ec0d392ff4
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-29 20:39:49 +0100

    chore: rewrite command files to ts

---
+++ src/commands/utility/server.ts
@@ -0,0 +1,11 @@
+import {ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder} from 'discord.js';
+
+export const command = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Provides information about the server.'),
+    async execute (interaction: ChatInputCommandInteraction) {
+        await interaction.reply(`This server is ${interaction.guild?.name} and has ${interaction.guild?.memberCount} members.`);
+    }
+
+}
