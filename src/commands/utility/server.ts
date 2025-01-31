0c80b042d035d5fd9ebe182d311cbf6df65b8372
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-31 02:07:06 +0100

    fix: Change server.ts to only run in Guild context
    Note: I assume interaction.guild can be null, when the bot runs outside a guild context

--- src/commands/utility/server.ts
+++ src/commands/utility/server.ts
@@ -1,11 +1,16 @@
-import {ChatInputCommandInteraction, SlashCommandBuilder} from 'discord.js';
+import {ChatInputCommandInteraction, InteractionContextType, SlashCommandBuilder} from 'discord.js';
 
 export const command = {
     data: new SlashCommandBuilder()
         .setName('server')
-        .setDescription('Provides information about the server.'),
-    async execute (interaction: ChatInputCommandInteraction) {
-        await interaction.reply(`This server is ${interaction.guild?.name} and has ${interaction.guild?.memberCount} members.`);
+        .setDescription('Provides information about the server.')
+        .setContexts(InteractionContextType.Guild),
+    async execute(interaction: ChatInputCommandInteraction) {
+        if (!interaction.guild) {
+            await interaction.reply("Something went wrong");
+        } else {
+            await interaction.reply(`This server is ${interaction.guild.members} and has ${interaction.guild.memberCount} members.`);
+        }
     }
 
 }
