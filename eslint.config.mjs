2144410f2f86672a1a188c55f1b99a10cb0b2192
hegedus-mark <hegedusmark2018@gmail.com>
2025-01-31 03:48:06 +0100

    fix: replace previous eslint config with working version
    The previous eslint config wasn't configured properly for flat config, so I had to fix that.

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config[]} */
-export default {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true,
-}
