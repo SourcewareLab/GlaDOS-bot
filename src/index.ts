233748d6645a985c962bf39b1e902dd0283815cb
hegedus-mark <hegedusmark2018@gmail.com>
2025-02-05 11:04:34 +0100

    chore: Add dev commands to package.json

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
-import { ChatInputCommandInteraction, Client, Collection, Events, GatewayIntentBits, MessageFlags } from "discord.js";
+import {ChatInputCommandInteraction, Client, Collection, Events, GatewayIntentBits, MessageFlags} from "discord.js";
 import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
+import {fileURLToPath, pathToFileURL} from 'url';
+import {AppDatabase} from "./data/database.js";
 
-type Command = {
-  execute(interaction: ChatInputCommandInteraction): Promise<void>;
+
+export type AppChatInputCommandInteraction = ChatInputCommandInteraction & {
+    client: AppClient;
 }
 
-class ClientWithCommands extends Client {
-  commands: Collection<string, unknown>
+type Command = {
+    execute(interaction: AppChatInputCommandInteraction): Promise<void>;
+}
 
-  constructor() {
-    super({
-      intents: [
-        GatewayIntentBits.Guilds,
-        // required for Event GuildMemberAdd
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildMembers
-      ]
-    });
-    this.commands = new Collection();
-  }
+class AppClient extends Client {
+    commands: Collection<string, Command>
+    db: AppDatabase;
+
+    constructor() {
+        super({intents: [
+            GatewayIntentBits.Guilds,
+            // required for Event GuildMemberAdd
+            GatewayIntentBits.GuildPresences,
+            GatewayIntentBits.GuildMembers
+          ]});
+        this.commands = new Collection();
+        this.db = AppDatabase.getInstance();
+    }
+
+    destroy() {
+        return super.destroy();
+    }
 }
 
 const token = process.env.TOKEN;
-const client = new ClientWithCommands();
+const client = new AppClient();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -33,19 +43,21 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file =>
+        file.endsWith('.js') || file.endsWith('.ts')
+    );
 
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const { command } = await import(pathToFileURL(filePath).toString());
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const {command} = await import(pathToFileURL(filePath).toString());
 
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" propriety.`);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" propriety.`);
+        }
     }
-  }
 }
 
 // Dynamically load event handlers from the 'events' folder
@@ -62,30 +74,37 @@ for (const file of eventFiles) {
 }
 
 client.once(Events.ClientReady, readyClient => {
-  console.log('Ready!', `Logged in as ${readyClient.user.tag}`);
+    console.log('Ready!', `Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
 
-  const command = client.commands.get(interaction.commandName) as Command;
+    const typedInteraction = interaction as AppChatInputCommandInteraction;
+    const command = typedInteraction.client.commands.get(interaction.commandName) as Command;
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-    } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
     }
-  }
+
+    try {
+        await command.execute(typedInteraction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral
+            });
+        }
+    }
 });
 
 client.login(token);
