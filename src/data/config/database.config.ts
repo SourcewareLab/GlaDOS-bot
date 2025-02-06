940aaaa6a3b75e042a72311b53e290d6c02e8c8e
hegedus-mark <hegedusmark2018@gmail.com>
2025-02-06 17:38:12 +0100

    chore: Add missing env variables and assign default values

--- src/data/config/database.config.ts
+++ src/data/config/database.config.ts
@@ -1,9 +1,12 @@
+//most of these have a default value, so it's not necessary to set env variables for these.
+//The only exception is password, because the postgres server will need you to set up a password, so those 2 should be in sync
+
 export default {
-    username: process.env.DB_USERNAME ?? throwError('DB_USERNAME'),
+    username: process.env.DB_USERNAME = "postgres",
     password: process.env.DB_PASSWORD ?? throwError('DB_PASSWORD'),
-    database: process.env.DB_NAME ?? throwError('DB_NAME'),
-    host: process.env.DB_HOSTNAME ?? throwError('DB_HOSTNAME'),
-    port: parseInt(process.env.DB_PORT ?? throwError('DB_PORT')),
+    database: process.env.DB_NAME = "glados_dev_db",
+    host: process.env.DB_HOSTNAME = "localhost",
+    port: parseInt(process.env.DB_PORT = "5432"),
     dialect: "postgres",
 } as const;
 
