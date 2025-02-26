644cc23c93da45f53f81bebc4f25efca589e6a19
A-Cer23 <aricerrahyan@gmail.com>
2025-02-26 15:16:51 -0500

    refactor: move role add and remove to subcommands directory

--- src/commands/role/role.ts
+++ src/commands/role/role.ts
@@ -1,12 +1,9 @@
 import {
   ChatInputCommandInteraction,
-  GuildMember,
-  MessageFlags,
-  PermissionFlagsBits,
-  Role,
-  SlashCommandBuilder,
-  RESTJSONErrorCodes,
+  PermissionFlagsBits,  SlashCommandBuilder,
 } from "discord.js";
+import { addRole } from "./subcommands/addRole.js";
+import { removeRole } from "./subcommands/removeRole.js";
 
 export const command = {
   data: new SlashCommandBuilder()
@@ -57,147 +54,12 @@ export const command = {
 
     switch (subCommand) {
       case "add":
-        add(interaction);
+        // add(interaction);
+        await addRole(interaction)
         break;
       case "remove":
-        remove(interaction);
+        await removeRole(interaction);
         break;
     }
   },
 };
-
-async function add(interaction: ChatInputCommandInteraction) {
-  // Check if client has bot scope
-  if (!interaction.guild) {
-    interaction
-      .reply("Missing 'bot' scope")
-      .catch((err) =>
-        console.error(`Error: Replying to missing bot scope -> ${err}`),
-      );
-    return;
-  }
-
-  const role = getRole(interaction);
-  const user = getUser(interaction);
-
-  // Check if user already has the role
-  if (user.roles.cache.has(role.id)) {
-    interaction
-      .reply({
-        content: `'${user.displayName}' already has '${role.name}'`,
-        flags: MessageFlags.Ephemeral,
-      })
-      .catch((err) =>
-        console.error(
-          `Error: Replying to user having a role already -> ${err}`,
-        ),
-      );
-    return;
-  }
-
-  // Add the role to the user, then give success message to user
-  // If the client adding the role has less permission than the role being added, catch and log the error
-  interaction.guild.members
-    .addRole({
-      role: role,
-      user: user,
-    })
-    .then(() => {
-      console.log(
-        `Log: '${role.name}' role has been added to '${user.displayName}' by ${interaction.user.displayName}`,
-      );
-
-      interaction.reply({
-        content: `'${role.name}' role has been added to '${user.displayName}'`,
-        flags: MessageFlags.Ephemeral,
-      });
-    })
-    .catch(async (err) => {
-      console.error(`Error: Adding '${role.name}' role to a member -> ${err}`);
-
-      if (err.code === RESTJSONErrorCodes.MissingPermissions) {
-        interaction.reply({
-          content: `Error: '${role.name}' could not be assigned. Tip: the role might have higher permissions than GlaDOS-bot's role`,
-          flags: MessageFlags.Ephemeral,
-        });
-      } else {
-        interaction.reply({
-          content: `Error: ${err}`,
-          flags: MessageFlags.Ephemeral,
-        });
-      }
-    });
-}
-
-async function remove(interaction: ChatInputCommandInteraction) {
-  // Check if client has bot scope
-  if (!interaction.guild) {
-    interaction
-      .reply("Missing 'bot' scope")
-      .catch((err) =>
-        console.error(`Error: Replying to missing bot scope -> ${err}`),
-      );
-    return;
-  }
-
-  const role = getRole(interaction);
-  const user = getUser(interaction);
-
-  // Check if user does not have the role already
-  if (!user.roles.cache.has(role.id)) {
-    interaction
-      .reply({
-        content: `'${user.displayName}' does not have '${role.name}'`,
-        flags: MessageFlags.Ephemeral,
-      })
-      .catch((err) =>
-        console.error(
-          `Error: Replying to user not having a role already -> ${err}`,
-        ),
-      );
-    return;
-  }
-
-  // Remove the role to the user, then give success message to user
-  // If the client adding the role has less permission than the role being removed, catch and log the error
-  interaction.guild.members
-    .removeRole({
-      role: role,
-      user: user,
-    })
-    .then(() => {
-      console.log(
-        `Log: '${role.name}' role has been removed from '${user.displayName}' by ${interaction.user.displayName}`,
-      );
-
-      interaction.reply({
-        content: `'${role.name}' role has been removed from '${user.displayName}'`,
-        flags: MessageFlags.Ephemeral,
-      });
-    })
-    .catch(async (err) => {
-      console.error(
-        `Error: Removing '${role.name}' role from member -> ${err}`,
-      );
-
-      if (err.code === RESTJSONErrorCodes.MissingPermissions) {
-        interaction.reply({
-          content: `Error: '${role.name}' could not be removed. Tip: the role might have higher permissions than GlaDOS-bot's role`,
-          flags: MessageFlags.Ephemeral,
-        });
-      } else {
-        interaction.reply({
-          content: `Error: ${err}`,
-          flags: MessageFlags.Ephemeral,
-        });
-      }
-    });
-}
-
-function getRole(interaction: ChatInputCommandInteraction): Role {
-  return interaction.options.getRole("role") as Role;
-}
-
-function getUser(interaction: ChatInputCommandInteraction): GuildMember {
-  return interaction.options.getMember("user") as GuildMember;
-}
---
+++ src/commands/role/subcommands/removeRole.ts
@@ -0,0 +1,67 @@
+import { ChatInputCommandInteraction, GuildMember, MessageFlags, RESTJSONErrorCodes, Role } from "discord.js";
+
+export async function removeRole(interaction: ChatInputCommandInteraction) {
+  // Check if client has bot scope
+  if (!interaction.guild) {
+    interaction
+      .reply("Missing 'bot' scope")
+      .catch((err) =>
+        console.error(`Error: Replying to missing bot scope -> ${err}`),
+      );
+    return;
+  }
+
+    const role = interaction.options.getRole("role") as Role;
+    const user = interaction.options.getMember("user") as GuildMember;
+
+
+  // Check if user does not have the role already
+  if (!user.roles.cache.has(role.id)) {
+    interaction
+      .reply({
+        content: `'${user.displayName}' does not have '${role.name}'`,
+        flags: MessageFlags.Ephemeral,
+      })
+      .catch((err) =>
+        console.error(
+          `Error: Replying to user not having a role already -> ${err}`,
+        ),
+      );
+    return;
+  }
+
+  // Remove the role to the user, then give success message to user
+  // If the client adding the role has less permission than the role being removed, catch and log the error
+  interaction.guild.members
+    .removeRole({
+      role: role,
+      user: user,
+    })
+    .then(() => {
+      console.log(
+        `Log: '${role.name}' role has been removed from '${user.displayName}' by ${interaction.user.displayName}`,
+      );
+
+      interaction.reply({
+        content: `'${role.name}' role has been removed from '${user.displayName}'`,
+        flags: MessageFlags.Ephemeral,
+      });
+    })
+    .catch(async (err) => {
+      console.error(
+        `Error: Removing '${role.name}' role from member -> ${err}`,
+      );
+
+      if (err.code === RESTJSONErrorCodes.MissingPermissions) {
+        interaction.reply({
+          content: `Error: '${role.name}' could not be removed. Tip: the role might have higher permissions than GlaDOS-bot's role`,
+          flags: MessageFlags.Ephemeral,
+        });
+      } else {
+        interaction.reply({
+          content: `Error: ${err}`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    });
+}
